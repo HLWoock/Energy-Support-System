6e24cf272937fe93ff6cafe5969160a32ecf558a
HermannW <HermannW@NOOSE1099.oosede.local>
2020-03-05 22:14:41 +0100

    initial

---
+++ src/main/java/de/woock/EnergySupportSystemApplication.java
@@ -0,0 +1,13 @@
+package de.woock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergySupportSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergySupportSystemApplication.class, args);
+	}
+
+}
